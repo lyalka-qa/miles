fce46c48ace3ae01724b1827cac1a8b5d1667512
Alena Filatova <ppasd2005@yandex.ru>
2021-03-22 15:03:00 +0300

    Modified

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int amount = 5647;
         int bonus = 20;
         int miles = 1;
-        int bonusmiles = miles * amount / bonus;
-        System.out.println(bonusmiles);
+        int bonusMiles = miles * amount / bonus;
+        System.out.println(bonusMiles);
     }
 }
