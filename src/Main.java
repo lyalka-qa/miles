1d2744802c747b284c2edaf94587b24fcfbb57f9
Alena Filatova <ppasd2005@yandex.ru>
2021-03-22 09:31:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 5647;
+        int bonus = 20;
+        int miles = 1;
+        int bonusmiles = miles * amount / bonus;
+        System.out.println(bonusmiles);
+    }
+}
